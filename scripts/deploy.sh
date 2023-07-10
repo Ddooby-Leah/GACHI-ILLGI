d1ed998f44bdbd5725679d6b563627c6480b9edc
ddooby.doobob <ddooby.doobob@kakaoenterprise.com>
2023-07-10 20:29:29 +0900

    refact : deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 # 기존 컨테이너 삭제
-sudo docker rm -f gachi-diary-backend
+sudo docker rm -f gachi-backend
 
 # 기존 이미지 삭제
-sudo docker rmi gachi-diary-backend
+sudo docker rmi qkdrmsgh73/gachi-diary-backend
 
 # 도커 실행
 sudo docker run -d -p 8080:8080 --restart=unless-stopped --log-opt max-size=10m --log-opt max-file=3 --name gachi-backend qkdrmsgh73/gachi-diary-backend
